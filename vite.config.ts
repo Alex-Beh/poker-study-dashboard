7ee19e4d9d21b03c8c79e25345e1b83570173f91
Alex Beh <66585558+Alex-Beh@users.noreply.github.com>
2025-05-14 08:36:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/poker-study-dashboard/',
+  // base: '/poker-study-dashboard/',
   server: {
     host: "::",
     port: 8080,
